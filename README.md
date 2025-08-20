400242d3a24e72da7f5ba027a9ecd613c3295116
Auxidity <auxidity@gmail.com>
2025-08-20 19:24:17 +0300

    updated default, added some additional features

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # catbash
 Cat | Bash
+
+## Usage
+If you have a file that has the following typed inside it: echo "Hello, World!" 
+calling catbash hello_world.txt will result in 'catbashing' the content, resulting in cat echo "Hello, World!" | bash to be given to execute to the shell.
+
+Additionally, you can optionally specify arguments for more options.
+
+You can do something like
+
+catbash -i "ls"  -o out.txt -c -t test.txt -a "| grep Cargo"
+
+where we declare to use "ls" as the command, write it to out.txt for further use, -c to signify that the output of catbash should be captured, -t to store the output of captured output in test.txt, and -a is a custom argument which gets applied to the captured output, before it gets stored.
+
+So in this case, if this git is used as the place where you execute the above command, it would first list all the files, store the command 'ls' to out.txt, then grep for Cargo on the result of invoking ls, which would result in Cargo.lock\nCargo.toml\n . This is then stored to test.txt .
+
+## Why?
+I have found to be typing cat somefile.txt | bash repeatedly over the years to automate scripts that I want to run often on demand, but not on a cronjob level.
+
+For example, I might have a c program that I want to compile with loads of flags, and vast majority of the time its with the same flags. I like to write the entire thing into a text file and just cat | bash it.
+
+Another example is when I need to use curl for a webserver that I am debugging. I prefer to do my debugging from the CLI and not use fancy tools, so the ability to test endpoints from CLI the exact way I want to tends to be some form of curl -X POST localhost:6969/api/give_me_auth_key , and then wanting to store that auth key to do something like
+
+curl -X POST http://localhost:6969/api/blogs \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJvb3QiLCJpZCI6IjY3YjVmMTRlMjEyNDhjYjExMDQwNmMzMSIsImlhdCI6MTczOTk3NzA5MiwiZXhwIjoxNzM5OTgwNjkyfQ.UZxwxdExDRbzrCZbs0B79eHEljNtUUdFl-RqagT_sQM" \
+  -d '{
+    "title": "Sample Blog Title",
+    "author": "Sample Author",
+    "url": "http://sampleurl.com"
+  }'
+
+and automatically replace the Bearer field with whatever the first POST responds with.
+
+The solution leaves all responsibility for you to figure out how to handle the string processing, catbash just gives you the string as is (or stored to a file), and if you can figure out how to string process it from echo "{my output}", it allows you to do so. Alternatively you could manually modify the stored file, and then catbash that.
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::io::{self, Write};
 use std::path::Path;
 use std::process::Command;
 
+//ToDo: Handle "" parsing in strings (raw stringify).
+//E.g. echo "something" should behave weirdly.
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -15,8 +17,33 @@ struct Args {
     )]
     input: Option<String>,
 
-    #[arg(short = 'o', long, help = "Output file that will be catbashed")]
+    #[arg(
+        short = 'o',
+        long,
+        help = "Output file that will be catbashed. NOTE: THE OUTPUT FILE WILL BE OVERWRITTEN ON EVERY INVOCATION"
+    )]
     output: Option<String>,
+
+    #[arg(
+        short = 'c',
+        long,
+        help = "Used to signify the need to capture output of catbash"
+    )]
+    capture: bool,
+
+    #[arg(
+        short = 't',
+        long,
+        help = "Target file where to store captured value. THIS FILE WILL BE OVERWRITTEN ON EVERY INVOCATION"
+    )]
+    target: Option<String>,
+
+    #[arg(
+        short = 'a',
+        long,
+        help = "Post capture modification arguments. E.g if catbash runs ls, argument could be '| grep something', and only matches with something would be stored to target."
+    )]
+    arguments: Option<String>,
 
     // Positional arguments (non-flag arguments)
     #[arg(help = "Files to process with default behavior. Only 1 is expected for now")]
@@ -31,27 +58,106 @@ enum AppMode {
     Error(String),
 }
 
-fn catbash(path: &str) {
+fn execute_arguments(
+    captured_value: &str,
+    arguments: &str,
+    write_to_stdout: bool,
+) -> Result<String, Box<dyn Error>> {
+    let cmd = format!(r#"echo "{captured_value}" {arguments}"#);
+
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(&cmd)
+        .output()
+        .expect("Failed to catbash");
+
+    if !output.status.success() {
+        return Err("Command exited with non-zero status".into());
+    }
+
+    if write_to_stdout {
+        io::stdout().write_all(&output.stdout)?;
+        io::stderr().write_all(&output.stderr)?;
+    }
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+
+    Ok(stdout.to_string())
+}
+
+fn catbash(path: &str) -> Result<(), Box<dyn Error>> {
     let cmd = format!(r#"cat "{path}" | bash"#);
 
-    let status = Command::new("sh")
+    let output = Command::new("sh")
         .arg("-c")
         .arg(&cmd)
         .status()
         .expect("Failed to catbash");
 
-    if !status.success() {
-        eprintln!("Command exited with non-zero status");
+    if !output.success() {
+        return Err("Command exited with non-zero status".into());
     }
+
+    Ok(())
+}
+
+//There is no winning on this particular function. If you call capture catbash with ls, it works
+//better with args flags (other shell commands) when we use the following. If we wanted to emulate
+//what regular catbash does (in terms of formatting the output with \t instead of \n in the case of
+//ls, which is handled by ls internally), the execute_arguments() becomes brittle immediately.
+fn capture_catbash(path: &str, write_to_stdout: bool) -> Result<String, Box<dyn Error>> {
+    let cmd = format!(r#"cat "{path}" | bash"#);
+
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(&cmd)
+        .output()
+        .expect("Failed to catbash");
+
+    if !output.status.success() {
+        return Err("Command exited with non-zero status".into());
+    }
+
+    if write_to_stdout {
+        io::stdout().write_all(&output.stdout)?;
+        io::stderr().write_all(&output.stderr)?;
+    }
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+
+    Ok(stdout.to_string())
 }
 
 fn has_flags_from_matches(matches: &ArgMatches) -> bool {
-    matches.args_present()
+    //Regardless of if files is recognized as a non-flag argument or not, which seems to vary on the day, this should work.
+    matches.args_present() && !matches.contains_id("files")
+}
+
+fn validate_matches(matches: &ArgMatches, args_given: &Args) -> Result<(), Box<dyn Error>> {
+    if args_given.capture && !matches.contains_id("output") {
+        return Err("Capture flag requires an output file to catbash".into());
+    };
+
+    if matches.contains_id("target") && (!matches.contains_id("output") || !args_given.capture) {
+        return Err(
+            "Target flag requires an output file to catbash and capture flag to be present".into(),
+        );
+    };
+
+    if matches.contains_id("arguments") && (!matches.contains_id("output") || !args_given.capture) {
+        return Err(
+            "Arguments flag requires an output file to catbash and capture flag to be present"
+                .into(),
+        );
+    }
+
+    Ok(())
 }
 
 fn determine_app_mode(args: &Args) -> Result<AppMode, Box<dyn Error>> {
     let cmd = Args::command();
     let matches = cmd.try_get_matches()?;
+    validate_matches(&matches, args)?;
 
     let has_flags = has_flags_from_matches(&matches);
     let files: Vec<String> = matches
@@ -59,6 +165,10 @@ fn determine_app_mode(args: &Args) -> Result<AppMode, Box<dyn Error>> {
         .unwrap_or_default()
         .cloned()
         .collect();
+
+    //Debug
+    // println!("{matches:?}");
+    // println!("{has_flags:?}");
 
     Ok(match (has_flags, files.len()) {
         (false, 0) => AppMode::NoArgs,
@@ -82,29 +192,67 @@ fn handle_no_args() -> Result<(), Box<dyn Error>> {
 }
 
 fn handle_default_behavior(filename: &str) -> Result<(), Box<dyn Error>> {
-    catbash(filename);
+    catbash(filename)?;
     Ok(())
 }
 
+//This function is a clusterfuck. I cant think of anything more clear than this though to achieve
+//the same functionality.
 fn handle_defined_flags(args: &Args) -> Result<(), Box<dyn Error>> {
-    if let (Some(input), None) = (&args.input, &args.output) {
-        println!(
-            "Avoid using -i flag alone, the application might not be able to remove the temporary file created to execute"
-        );
+    match (
+        &args.input,
+        &args.output,
+        &args.capture,
+        &args.target,
+        &args.arguments,
+    ) {
+        (Some(input), None, false, None, None) => {
+            println!(
+                "Avoid using -i flag alone, the application might not be able to create or remove the temporary file created to execute"
+            );
 
-        let temp_path = "temp.txt";
-        write_to_file(input, temp_path)?;
-        catbash(temp_path);
-        delete_file(temp_path)?;
-    }
-
-    if let (None, Some(output)) = (&args.input, &args.output) {
-        catbash(output);
-    }
-
-    if let (Some(input), Some(output)) = (&args.input, &args.output) {
-        write_to_file(input, output)?;
-        catbash(output);
+            let temp_path = "temp.txt";
+            write_to_file(input, temp_path)?;
+            catbash(temp_path)?;
+            delete_file(temp_path)?;
+        }
+        (None, Some(output), false, None, None) => {
+            catbash(output)?;
+        }
+        (Some(input), Some(output), false, None, None) => {
+            write_to_file(input, output)?;
+            catbash(output)?;
+        }
+        (Some(input), Some(output), true, None, None) => {
+            write_to_file(input, output)?;
+            let _captured_value = capture_catbash(output, true)?;
+        }
+        (Some(input), Some(output), true, Some(target), None) => {
+            write_to_file(input, output)?;
+            let captured_value = capture_catbash(output, false)?;
+            write_to_file(&captured_value, target)?;
+        }
+        (None, Some(output), true, None, Some(arguments)) => {
+            let captured_value = capture_catbash(output, false)?;
+            let _modified_value = execute_arguments(&captured_value, arguments, true)?;
+        }
+        (None, Some(output), true, Some(target), Some(arguments)) => {
+            let captured_value = capture_catbash(output, false)?;
+            let modified_value = execute_arguments(&captured_value, arguments, false)?;
+            write_to_file(&modified_value, target)?;
+        }
+        (Some(input), Some(output), true, None, Some(arguments)) => {
+            write_to_file(input, output)?;
+            let captured_value = capture_catbash(output, false)?;
+            let _modified_value = execute_arguments(&captured_value, arguments, true)?;
+        }
+        (Some(input), Some(output), true, Some(target), Some(arguments)) => {
+            write_to_file(input, output)?;
+            let captured_value = capture_catbash(output, false)?;
+            let modified_value = execute_arguments(&captured_value, arguments, false)?;
+            write_to_file(&modified_value, target)?;
+        }
+        _ => todo!(),
     }
 
     Ok(())
@@ -120,7 +268,11 @@ fn write_to_file(content: &str, dest: &str) -> io::Result<()> {
         fs::write(dest, "")?; // create the file if it doesn't exist
     }
 
-    let mut file = OpenOptions::new().append(false).write(true).open(dest)?;
+    let mut file = OpenOptions::new()
+        .append(false)
+        .write(true)
+        .truncate(true)
+        .open(dest)?;
     file.write_all(content.as_bytes())?;
 
     Ok(())
